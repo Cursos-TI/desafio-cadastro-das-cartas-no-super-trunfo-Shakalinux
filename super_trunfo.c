1c26bcaa388e0da833e85e45404efca5dfa614ab
Henrique Rocha Pereira <hrprocha3@outlook.com>
2025-04-10 01:34:49 +0000

    Nova versão do nível aventureiro.

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,18 +4,20 @@ int main() {
     char estado, segundoEstado, codigo[5], segundoCodigo[5], cidade[50], segundaCidade[50];
     int populacao, segundaPopulacao, pontos, segundoPontos;
     float area, segundaArea, pib, segundoPib;
+    float desensidade, segundaDesensidade;
+    float pibPercap, segundoPibPercap;
 
-  
     printf("Informe os dados da primeira carta:\n");
-    
+
     printf("Estado (letra de A a H): ");
-    scanf(" %c", &estado);  
+    scanf(" %c", &estado);
 
     printf("Código da Carta (número de 1 a 4 | ex: A01): ");
     scanf("%s", codigo);
 
     printf("Nome da Cidade: ");
-    scanf(" %[^\n]", cidade);  
+    scanf(" %[^\n]", cidade);
+
     printf("População: ");
     scanf("%d", &populacao);
 
@@ -23,22 +25,24 @@ int main() {
     scanf("%f", &area);
 
     printf("PIB (em bilhões de reais): ");
-    scanf("%f", &pib);
+    scanf("%f", &pib); 
 
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontos);
 
   
+    desensidade = (float)populacao / area;
+    pibPercap = (pib * 1000000000) / (float)populacao; 
     printf("\nInforme os dados da segunda carta:\n");
 
     printf("Estado (letra de A a H): ");
-    scanf(" %c", &segundoEstado);  
+    scanf(" %c", &segundoEstado);
 
     printf("Código da Carta (número de 1 a 4 | ex: A01): ");
     scanf("%s", segundoCodigo);
 
     printf("Nome da Cidade: ");
-    scanf(" %[^\n]", segundaCidade);  
+    scanf(" %[^\n]", segundaCidade);
 
     printf("População: ");
     scanf("%d", &segundaPopulacao);
@@ -47,10 +51,14 @@ int main() {
     scanf("%f", &segundaArea);
 
     printf("PIB (em bilhões de reais): ");
-    scanf("%f", &segundoPib);
+    scanf("%f", &segundoPib); 
 
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &segundoPontos);
+
+   
+    segundaDesensidade = (float)segundaPopulacao / segundaArea;
+    segundoPibPercap = (segundoPib * 1000000000) / (float)segundaPopulacao; 
 
     
     printf("\nCarta 1:\n");
@@ -61,8 +69,10 @@ int main() {
     printf("Área: %.2f km²\n", area);
     printf("PIB: %.2f bilhões de reais\n", pib);
     printf("Número de Pontos Turísticos: %d\n", pontos);
+    printf("Densidade Populacional: %.2f hab/km²\n", desensidade);
+    printf("PIB per Capita: %.2f reais\n", pibPercap);
 
-    
+   
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", segundoEstado);
     printf("Código: %s\n", segundoCodigo);
@@ -71,6 +81,8 @@ int main() {
     printf("Área: %.2f km²\n", segundaArea);
     printf("PIB: %.2f bilhões de reais\n", segundoPib);
     printf("Número de Pontos Turísticos: %d\n", segundoPontos);
+    printf("Densidade Populacional: %.2f hab/km²\n", segundaDesensidade);
+    printf("PIB per Capita: %.2f reais\n", segundoPibPercap);
 
     return 0;
 }
